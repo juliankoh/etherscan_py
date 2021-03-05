e91c9c63a3a3a26d028ce8db7b51ffc8c1fee978
Julian Koh <jk2698@cornell.edu>
2021-03-05 13:44:52 +0000

    version

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/juliankoh/etherscan_py',
-    version='0.2.3',
+    version='0.2.4',
     zip_safe=False,
 )
